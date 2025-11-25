3d9cf47c1b3bc31df16fb09582e7732462ae44b4
Rodion Timin <admin@MacBook-Air-Rodion.local>
2025-11-25 19:59:20 +0300

    создание класса User

--- src/main/java/ru/max/User.java
+++ src/main/java/ru/max/User.java
@@ -11,8 +11,31 @@ public class User {
         this.password = password;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void sendMessage(Message message) {
         // Вызываем putMessageInArray(). Т.е. кладём сообщение в массив сообщений
     }
-
 }
