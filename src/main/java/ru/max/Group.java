7f7536b51009c276c0f46971beea71dd2391a9af
rifatval <rifat.val.b@gmail.com>
2025-11-25 20:42:50 +0300

    Выполнен класс Group

--- src/main/java/ru/max/Group.java
+++ src/main/java/ru/max/Group.java
@@ -4,22 +4,37 @@ import java.util.Arrays;
 
 public class Group {
     private String name;
-    private User[] users;
+    private User[] users = new User[100];
+    private int countUsers = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Group(String name, User[] users) {
         this.name = name;
-        this.users = users;
+        this.countUsers = users.length;
+        for (int i = 0; i < countUsers; i++) {
+            this.users[i] = users[i];
+        }
     }
 
     public void addUser(User user) {
-
+        if (countUsers < users.length) {
+            users[countUsers++] = user;
+        }
     }
 
     public void sendMessageInGroup(Message message) {
         // Вызываем Message.putMessageInArray()
+        Message.putMessageInArray(message);
     }
 
     public String toString() {
-        return "";
+        return "Группа: " + name + "\n\nПользователи:\n" + Arrays.toString(users) + "\n";
     }
 }
